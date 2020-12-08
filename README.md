010f11f3f7044103c03c348500cf0146079a4fdf
rudyjm3 <rudyjm3@yahoo.com>
2020-12-08 13:53:22 -0500

    display value of the button clicked

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Resources:
 
 Animated button effect: https://www.w3schools.com/howto/howto_css_animate_buttons.asp
 https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_buttons_animate2
+
+How to get the value assigned to a button: https://stackoverflow.com/questions/40134104/how-to-pass-the-button-value-into-my-onclick-event-function
--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 
-function testFunction (){
+//DISPLAY 1ST QUESTION ONLY
 
-document.getElementsByTagName('button').clicked == true;
-alert("Button clicked")
+function answerDisplay (val){
+  //let answer = document.getElementById('yes-btn no-btn').value;
+  document.getElementById('answer').innerHTML = "("+ val + ")";
+  
+  console.log(answer);
 }
+//WHEN QUESTION ANSWERED DISPLAY ANSWER INSIDE THE QUESTION AND SHOW THE NEXT // QUESTION
+
+
+//IF WRONG ANSWER IS PICKED SHOW "FAILED ALERT TEXT"
+
+
+// ONCE LAST QUESTION IS ANSWERED DISPLAY SUBMIT BUTTON SECTION AND SUBMIT alert (passed-alert-text)
+
+
+// ONCE FROM IS SUBMITTED SHOW SUCCESS ALERT TEXT
