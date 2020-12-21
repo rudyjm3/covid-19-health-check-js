926d11b72b615137c155c7a832715d90bb2367d9
rudyjm3 <rudyjm3@yahoo.com>
2020-12-21 00:38:35 -0500

    main.js added to function answerDisplay

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 
 //DISPLAY 1ST QUESTION ONLY
 
+//WHEN QUESTION ANSWERED DISPLAY ANSWER INSIDE THE QUESTION AND SHOW THE NEXT // QUESTION
 function answerDisplay (val){
   //let answer = document.getElementById('yes-btn no-btn').value;
-  document.getElementById('answer').innerHTML = "("+ val + ")";
-  
+  let answeredQuestion = document.getElementsByClassName('active-question')[0];
+  let answeredBtns = document.getElementsByClassName('buttons-group')[0];
+  if (val == "No") {
+    alert("Great");
+    document.getElementById('answer').innerHTML = "("+ val + ")";
+    answeredQuestion.classList.add('answered-question');
+    answeredBtns.classList.add('buttons-group-answered');
+  } else {
+    alert("No Good");
+    document.getElementById('answer').innerHTML = "("+ val + ")";
+    answeredQuestion.classList.add('answered-question');
+    answeredBtns.classList.add('buttons-group-answered');
+  }
+
+
+
   console.log(answer);
+
 }
-//WHEN QUESTION ANSWERED DISPLAY ANSWER INSIDE THE QUESTION AND SHOW THE NEXT // QUESTION
+
 
 
 //IF WRONG ANSWER IS PICKED SHOW "FAILED ALERT TEXT"
